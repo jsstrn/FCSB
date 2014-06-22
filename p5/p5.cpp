24213e5189129ef3f78011eb04c7f5ecf6c14d04
Mohafizz <mohafizz@gmail.com>
2014-06-22 13:11:35 +0800

    So far everything works
    I am not sure if we should literally use what is asked by the asked for
    in the question:
    
    e.g. listAddFront(node* &head, node* newNode)

--- p5/p5.cpp
+++ p5/p5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 struct Node
@@ -28,14 +29,15 @@ Node* listDelete(Node* &, int);
 Node* listCreate(int);
 
 /* Displays each node in a list */
-void listDisplay(Node*);
+void listDisplay(Node* &);
 
 /* * * * * * * Main Function * * * * * * */
 
 int main()
 {
-	// create 10 nodes
-	Node* firstNode = listCreate(10);
+	//create 10 nodes
+	//Node* firstNode = listCreate(10);
+	Node* firstNode = listCreate(0);   //To test building an empty list
 	listDisplay(firstNode);
 
 	/* Q1 — Find the length of a list  */
@@ -50,8 +52,11 @@ int main()
 	listDisplay(firstNode);
 
 	/* Q3 — Find a node in a list */
-	Node* findNode = listFind(firstNode, 5);
-	listDisplay(findNode);
+	Node* findNode = listFind(firstNode, 11);
+	if(findNode!=NULL) cout << "Node "<< findNode->data << " is found!" << endl;
+	else cout << "Node is not in the list!" << endl;
+	//else listDisplay(findNode);
+
 
 	/* Q4 — Add a new node at the tail of a list  */
 	Node* backNode = new Node();
@@ -61,7 +66,9 @@ int main()
 	listDisplay(firstNode);
 
 	/* Q5 — Delete a node from a list */
-	listDelete(firstNode, 3);
+	Node* deleteNode =listDelete(firstNode, 99);
+	if(deleteNode!=NULL) cout << "Node "<< deleteNode->data << " is found and deleted!" << endl;
+	else cout << "Node is not in the list!" << endl;
 	listDisplay(firstNode);
 
 	delete firstNode;
@@ -94,7 +101,7 @@ Node* listFind(Node* firstNode, int value) {
 	Node* currentNode = firstNode;
 	while (currentNode != NULL) {
 		if (currentNode->data == value) {
-			currentNode->next = NULL;
+			//currentNode->next = NULL;        //this is the culprit
 			return currentNode;
 		}
 		currentNode = currentNode->next;
@@ -148,13 +155,14 @@ Node* listCreate(int size) {
 }
 
 /* Displays each node in a list */
-void listDisplay(Node* firstNode) {
+void listDisplay(Node* &firstNode) {
 	Node* currentNode = firstNode;
 	if (currentNode == NULL) cout << "List is empty!";
 	while (currentNode != NULL) {
 		cout << currentNode->data;
 		if (currentNode->next != NULL) cout << " -> ";
 		currentNode = currentNode->next;
+		
 	}
 	cout << endl;
 }
